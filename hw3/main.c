cb537366562ee58a53a789d90d27cc88a405e03f
Olivia Li <oliviazli6204@gmail.com>
2025-04-08 13:36:18 -0500

    tested external button and LED circuit

--- hw3/main.c
+++ hw3/main.c
@@ -5,16 +5,31 @@
 int main()
 {
     stdio_init_all();
+
     while (!stdio_usb_connected()) {
         sleep_ms(100);
     }
-    
+
     printf("Start!\n");
- 
+    
+    // initalize the LED on the PICO
+    gpio_init(25);
+    gpio_set_dir(25, GPIO_OUT);
+
+    // initalize the button 
+    gpio_init(22);
+    gpio_set_dir(22, GPIO_IN);
+
     while (1) {
-        char message[100];
-        scanf("%s", message);
-        printf("message: %s\r\n",message);
-        sleep_ms(50);
+        if (gpio_get(22) == 0) {
+            gpio_put(25, 1);
+        } else {
+            gpio_put(25, 0);
+        }
+
+        // char message[100];
+        // scanf("%s", message);
+        // printf("message: %s\r\n",message);
+        // sleep_ms(50);
     }
 }
